655d97db3de267250af268d4c73a6260cafd5439
20sanjana39 <48740308+20sanjana39@users.noreply.github.com>
2019-04-01 14:55:45 +0900

    Title & BGColor
    Added name to title and changed bgcolour to black

--- capstone.py
+++ capstone.py
@@ -1,8 +1,8 @@
 import turtle 
 
 wn = turtle.Screen()
-wn.title("Jumping Avenger")
+wn.title("Jumping Avenger by Sanjana")
 wn.setup(height = 600, width = 800)
-wn.bgcolor("blue")
+wn.bgcolor("black")
 
 wn.mainloop()
